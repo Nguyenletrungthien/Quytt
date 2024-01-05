16d80b941d61fd25f3844432b7f2b42dd0991ce2
Nguyenletrungthien <101155715+Nguyenletrungthien@users.noreply.github.com>
2024-01-05 15:17:00 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,9 @@
+const CONFIGDATA = {
+  titleHeader: "Chào chị, em có điều muốn nói. </br> Chị làm người yêu em nhé",
+  descriptionHeader:"Hãy trả lời một cách thật lòng nhé. Nếu chị im lặng và rời đi thì tức là chị đã ĐỒNG Ý ^^",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: "Cảm ơn chị",
+  descriptionModar:
+    'Cảm ơn chị đã cho em một cơ hội để em có thể ở bên cạnh, che chở và bảo vệ chị. ^^ </br> Em biết em không được đẹp trai, cũng chẳng lắm tài nhưng em có một trái tim chân thành. Chị chính là cả thế giới của em. </br> Love you <3 "',
+};
